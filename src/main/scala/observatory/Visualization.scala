f391f4772f6fa118eca91171401b1060960f765a
André Gomes Lamas Otero <lamas3000@gmail.com>
2017-09-27 18:02:32 -0300

    Second milestone

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,11 +1,48 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import observatory.constants.{
+minDistanceMeters, p, earthRadiusMeters,
+imageWidth, imageHeight
+}
+
+import scala.math._
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization {
+
+  // From: https://en.wikipedia.org/wiki/Great-circle_distance
+  private[observatory] def distance(location1: Location, location2: Location): Double = {
+    val phi1 = toRadians(location1.lat)
+    val lambda1 = toRadians(location1.lon)
+    val phi2 = toRadians(location2.lat)
+    val lambda2 = toRadians(location2.lon)
+
+    val deltaLambda = abs(lambda1 - lambda2)
+    val deltaSigma = acos(sin(phi1) * sin(phi2) + cos(phi1) * cos(phi2) * cos(deltaLambda))
+
+    val distance = earthRadiusMeters * deltaSigma
+    distance // in meters
+  }
+  
+  private[observatory] def calculatePrediction(known: Iterable[(Location, Double)], forLocation: Location, p: Double = 2) = {
+    var weight, weightSum, weighedTemperatures = 0d
+
+    known.foreach {
+      case (loc, temperature) =>
+        val dist = distance(loc, forLocation)
+        weight =
+          if (dist <= minDistanceMeters) 1
+          else 1d / pow(dist, p)
+        weightSum += weight
+        weighedTemperatures += weight * temperature
+    }
+    weighedTemperatures / weightSum
+  }
+
+  
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
@@ -13,7 +50,7 @@ object Visualization {
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    ???
+     calculatePrediction(temperatures, location, p)
   }
 
   /**
@@ -22,16 +59,71 @@ object Visualization {
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
-    ???
+    val pointsSeq = points.toSeq.sortBy(_._1)
+    val bounds: ((Double, Color), (Double, Color)) = pointsSeq.indexWhere(_._1 >= value) match {
+      case -1 => // not found, value is after the right bound
+        (points.init.last, points.last) // return last 2 points
+      case 0 => // value is out of bounds on the left
+        (points.head, points.tail.head) // return first 2 points
+      case idx => // value is somewhere in bounds
+        (pointsSeq(idx - 1), pointsSeq(idx)) // return points surrounding the value
+    }
+
+    val color: Color = lerp(bounds._1, bounds._2, value)
+    color
   }
 
+  /*****/
+  private[observatory] def lerp(p1: (Double, Color), p2: (Double, Color), value: Double): Color = {
+    val (v1, Color(r1, g1, b1)) = p1
+    val (v2, Color(r2, g2, b2)) = p2
+
+    val r = lerp(v1, r1, v2, r2, value)
+    val g = lerp(v1, g1, v2, g2, value)
+    val b = lerp(v1, b1, v2, b2, value)
+
+    Color(r, g, b)
+  }
+
+  private[observatory] def lerp(v1: Double, c1: Int, v2: Double, c2: Int, v: Double): Int = {
+    val r = round(
+      c1 + (v - v1) * (c2 - c1) / (v2 - v1)
+    ).toInt
+    255 min r max 0
+  }
+
+  private[observatory] def rgbToPixel(color: Color, alpha: Int = 255): Pixel = {
+    val Color(r, g, b) = color
+    Pixel(r, g, b, alpha)
+  }
+
+  private[observatory] def idxToLoc(idx: Int, cols: Int): Location = {
+    val x = idx % cols
+    val y = idx / cols
+    val lon = x - 180
+    val lat =
+      if (y <= cols / 2) 90 - y
+      else -1 * (y - 90)
+    Location(lat, lon)
+  }
+  
+  /****/
+  
   /**
     * @param temperatures Known temperatures
     * @param colors Color scale
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)]): Image = {
-    ???
+    val pixels = (for {
+      i <- 0 until imageWidth * imageHeight
+      loc = idxToLoc(i, imageWidth)
+      temp = predictTemperature(temperatures, loc)
+      color = interpolateColor(colors, temp)
+      pixel = rgbToPixel(color)
+    } yield pixel).toArray
+
+    Image(imageWidth, imageHeight, pixels)
   }
 
 }
